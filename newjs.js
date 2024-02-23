83c695e7e3f637545f5f52dabeffbb5b2cc6cf6f
jairaj100 <160604208+jairaj100@users.noreply.github.com>
2024-02-23 17:27:40 +0530

    Update newjs.js

--- newjs.js
+++ newjs.js
@@ -1,10 +1,2 @@
 // This is a simple JavaScript code example
 // It alerts a message when the page is loaded
-
-// Wait for the DOM to be fully loaded
-//document.addEventListener("DOMContentLoaded", function() {
-    // Code inside this function will run when the DOM is ready
-    
-    // Alert a message
-    //alert("Hello, world!");
-//});
